98b2811975b3b8f0bc449dd4b8aa23b04b1d60a7
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-12-26 21:50:31 +0900

    hibiki.sh: porting from ruby to perl

--- bin/hibiki.sh
+++ bin/hibiki.sh
@@ -1,33 +1,43 @@
 #!/bin/sh -e
 # hibiki.sh ver.0.5.6 (2013-02-13)
-# require wget, ruby, and mimms
+# require wget, perl, and mimms
 #
 # http://interstadial.wordpress.com/2013/06/02/record-rtsp-streaming/
 # 2013/12/26
+#
+# from ruby to perl at 2013/12/26 by @xtetsuji
+
+#set -o xtrace # for DEBUG
 
 TMPFILE="/var/tmp/tmp.$$"
 trap 'rm -f ${TMPFILE}' EXIT
  
 if [ $# -eq 1 ]; then
-  STR=$1
+    STR=$1
  
-  # save asx file
-  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
+    # save asx file
+    #wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
+    wget -q -O - http://hibiki-radio.jp/description/${STR} | \
+        grep WMV | \
+        perl -ne 'm{src\s*=\s*"(http:.*?)"}x and print "$1\n"' | \
+        head -1 | \
+        xargs wget -q -O ${TMPFILE}
 
-  if [ -z ${TMPFILE} ]; then
-    echo "download ERROR"
-    exit 1
-  fi
-  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/title"].text'`
-  WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/Ref"].attributes["href"]'`
-  if test a"$TITLE" = a"" ; then
-    echo "asx detection failed."
-    exit 1
-  fi
+    if [ -z ${TMPFILE} ]; then
+        echo "download ERROR"
+        exit 1
+    fi
+    TITLE=`perl -ne 'm{<title>(.*?)</title>} and print "$1\n";' ${TMPFILE}`
+    WMVFILE=`perl -ne 'm{<Ref\s+href\s*=\s*"(.*?)"} and print "$1\n";' ${TMPFILE}`
+    
+    if test a"$TITLE" = a"" ; then
+        echo "asx detection failed."
+        exit 1
+    fi
  
-  mimms -r ${WMVFILE} "${TITLE}.asf"
-  mimms -r ${WMVFILE} "${TITLE}.asf"
+    mimms -r ${WMVFILE} "${TITLE}.asf"
+    mimms -r ${WMVFILE} "${TITLE}.asf"
  
 else
-  echo "usage: `basename $0` STRING"
+    echo "usage: `basename $0` STRING"
 fi
