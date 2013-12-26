322c2fd20e0f4af57584df94b069dc743eb51537
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2013-12-26 21:27:42 +0900

    Initial release: hibiki.sh from http://interstadial.wordpress.com/2013/06/02/record-rtsp-streaming/

---
+++ bin/hibiki.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+# hibiki.sh ver.0.5.6 (2013-02-13)
+# require wget, ruby, and mimms
+#
+# http://interstadial.wordpress.com/2013/06/02/record-rtsp-streaming/
+# 2013/12/26
+
+TMPFILE="/var/tmp/tmp.$$"
+trap 'rm -f ${TMPFILE}' EXIT
+ 
+if [ $# -eq 1 ]; then
+  STR=$1
+ 
+  # save asx file
+  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
+
+  if [ -z ${TMPFILE} ]; then
+    echo "download ERROR"
+    exit 1
+  fi
+  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/title"].text'`
+  WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/Ref"].attributes["href"]'`
+  if test a"$TITLE" = a"" ; then
+    echo "asx detection failed."
+    exit 1
+  fi
+ 
+  mimms -r ${WMVFILE} "${TITLE}.asf"
+  mimms -r ${WMVFILE} "${TITLE}.asf"
+ 
+else
+  echo "usage: `basename $0` STRING"
+fi
