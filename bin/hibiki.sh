ffbeb18cf9aaa0d3b4bb0317a074fd4d4aa07c3c
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2014-01-19 04:59:58 +0900

    hibiki.sh: Modify tiny.

--- bin/hibiki.sh
+++ bin/hibiki.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh
 # hibiki.sh ver.0.5.6 (2013-02-13)
 # require wget, perl, and mimms
+#
+# hibiki.sh CHANNEL_NAME
 #
 # http://interstadial.wordpress.com/2013/06/02/record-rtsp-streaming/
 # 2013/12/26
@@ -8,6 +10,7 @@
 # from ruby to perl at 2013/12/26 by @xtetsuji
 
 #set -o xtrace # for DEBUG
+set -e
 
 TMPFILE="/var/tmp/tmp.$$"
 trap 'rm -f ${TMPFILE}' EXIT
@@ -16,7 +19,6 @@ if [ $# -eq 1 ]; then
     STR=$1
  
     # save asx file
-    #wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
     wget -q -O - http://hibiki-radio.jp/description/${STR} | \
         grep WMV | \
         perl -ne 'm{src\s*=\s*"(http:.*?)"}x and print "$1\n"' | \
@@ -36,7 +38,7 @@ if [ $# -eq 1 ]; then
     fi
  
     mimms -r ${WMVFILE} "${TITLE}.asf"
-    mimms -r ${WMVFILE} "${TITLE}.asf"
+    mimms -r ${WMVFILE} "${TITLE}.2.asf"
  
 else
     echo "usage: `basename $0` STRING"
